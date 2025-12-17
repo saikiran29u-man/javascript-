29b05bdfd92ad1114648aeb66a76589a1a9d76dc
Sai kiran <saikiran29.u@gmail.com>
2025-12-17 18:27:43 +0530

    added dates.js file

---
+++ basics_js/08_dates.js
@@ -0,0 +1,24 @@
+
+let todayDate = new Date() //--> date is an object,it has many functions inside of it.
+console.log(todayDate)
+
+console.log(todayDate.toLocaleString()) //--> toLocaleString will give you date,month,year ,time in hr,min,sec.
+
+//console.log(todayDate.toLocaleDateString()) //--> toLocaleDateString will give you date,month,year.
+
+//console.log(todayDate.toString()) //-->converts the date into object .
+
+//console.log(todayDate.getDate()) //--> getDate() fun is used to print todays date.
+
+//console.log(todayDate.getDay() + 1)
+
+//console.log(todayDate.getMonth() +1)
+
+//console.log(todayDate.getMilliseconds()) //--> getMilliseconds() fun is used to print seconds.
+
+//console.log(todayDate.getMinutes()) //--> getMinutes() fun is used to print minutes.
+
+/*todayDate.toLocaleString('default',{
+    weekday:'narrow'
+
+}) --> we will study in detail in coming files. */
